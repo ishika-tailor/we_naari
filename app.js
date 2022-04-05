3429c9688d14160e678ff85057507d61cd4e9d30
Ishika <ishika@Ishikas-MacBook.local>
2022-04-05 14:17:47 +0530

    third commit

--- app.js
+++ app.js
@@ -23,5 +23,5 @@ app.use("/user",userRouter);
 
 const server = app.listen(process.env.PORT || 5000, () => {
   const port = server.address().port;
-  console.log(`Express is working on port ${port}`);
+  console.log("Express is working on port ${port}");
 });
